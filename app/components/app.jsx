0882140f0e5bf1a7d97fed3c786e1b8e8c9a0631
SeanSassenrath <sean.sassenrath@gmail.com>
2016-02-29 19:38:01 -0800

    Add basic nav bar

--- app/components/app.jsx
+++ app/components/app.jsx
@@ -6,11 +6,21 @@ require("../styles/style.scss");
 export default class App extends Component {
   render() {
     return (
-      <div className="row">
-        <div className="small-2 large-4 columns">...</div>
-        <div className="small-4 large-4 columns">...</div>
-        <div className="small-6 large-4 columns">...</div>
-      </div>
+      <nav style={{borderBottom: "1px solid gray"}}>
+        <div className="row" style={{border: "1px solid red"}}>
+          <div className="top-nav" style={{border: "1px solid yellow"}}>
+            <h3>FairThreads</h3>
+          </div>
+          <div className="bottom-nav" style={{border: "1px solid purple"}}>
+            <h3>Test</h3>
+          </div>
+        </div>
+      </nav>
+      // <div className="row">
+      //   <div className="small-2 large-4 columns">...</div>
+      //   <div className="small-4 large-4 columns">...</div>
+      //   <div className="small-6 large-4 columns">...</div>
+      // </div>
     )
   }
 }
