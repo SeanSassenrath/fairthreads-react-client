ae285ba4542865381b46243073476df3f80f51d4
SeanSassenrath <sean.sassenrath@gmail.com>
2016-03-06 13:53:03 -0800

    Add vendor link to product

--- app/components/product-card.jsx
+++ app/components/product-card.jsx
@@ -12,17 +12,19 @@ export default class ProductCard extends Component {
     return(
       <div className="small-6 large-3 columns">
         <div className="product" style={{width: "95%", margin: "0 auto", padding: "20px 0"}}>
-          <div style={{width: "89%", position: "relative", margin: "0 auto"}}>
-            <img src={product.imageOriginal} style={{width: "100%", display: "block", margin: "0 auto", objectFit: "contain" ,maxHeight:"281px", border: "1px solid gray"}} />
-            <h5 style={{background: "black", opacity: ".6", color: "white", width: "30%", textAlign: "right", position: "absolute", bottom: "5%", paddingRight: "8px"}}>${product.price}</h5>
-            {
-              product.salePrice ?
-                <h5 style={{background: "black", opacity: ".6", color: "white", width: "30%", textAlign: "right", position: "absolute", bottom: "5%", right: "0", paddingRight: "8px"}}>${product.salePrice}</h5>
-              : null
-            }
+          <a href={product.vendUrl}>
+              <div style={{width: "89%", position: "relative", margin: "0 auto"}}>
+                <img src={product.imageOriginal} style={{width: "100%", display: "block", margin: "0 auto", objectFit: "contain" ,maxHeight:"281px", border: "1px solid gray"}} />
+                <h5 style={{background: "black", opacity: ".6", color: "white", width: "30%", textAlign: "right", position: "absolute", bottom: "5%", paddingRight: "8px"}}>${product.price}</h5>
+                {
+                  product.salePrice ?
+                    <h5 style={{background: "black", opacity: ".6", color: "white", width: "30%", textAlign: "right", position: "absolute", bottom: "5%", right: "0", paddingRight: "8px"}}>${product.salePrice}</h5>
+                  : null
+                }
+              </div>
+              <h5 style={{textAlign: "center"}}>{product.name}</h5>
+            </a>
           </div>
-          <h5 style={{textAlign: "center"}}>{product.name}</h5>
-        </div>
       </div>
     )
   }
