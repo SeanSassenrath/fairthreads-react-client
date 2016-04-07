94f1dce0f56a72b97091a2ddb8725d8bc5ce3356
SeanSassenrath <sean.sassenrath@gmail.com>
2016-04-06 21:35:15 -0700

    Load products into product row component

--- src/components/products-container/products-container.jsx
+++ src/components/products-container/products-container.jsx
@@ -1,10 +1,13 @@
 import React, { Component, PropTypes } from 'react';
 import ProductRow from '../product-row/product-row.jsx';
 import ProductCard from '../product-card/product-card.jsx';
+import { connect } from 'react-redux';
+import { selectActions } from '../../selectors.js';
+import { testSelector } from '../../selectors.js';
 import { makeFourColumns } from '../../helpers.js';
 
 
-export default class ProductsContainer extends Component {
+class ProductsContainer extends Component {
 
   render() {
     let {
@@ -19,10 +22,12 @@ export default class ProductsContainer extends Component {
       <div id="products-container">
         {
           productRows.map(function(productRow ) {
-            return <Row key={rowKey++} row={productRow} />
+            return <ProductRow key={rowKey++} row={productRow} />
           })
         }
       </div>
     )
   }
 }
+
+export default connect(testSelector, selectActions)(ProductsContainer);
