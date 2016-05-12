bc2fe7135775a52189fb718721a9bb9f31376485
SeanSassenrath <sean.sassenrath@gmail.com>
2016-05-11 23:48:08 -0700

    Adjust package.json

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,48 +1,67 @@
-var webpack = require('webpack');
-var path = require('path');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var buildPath = path.resolve(__dirname, 'public', 'build');
-var mainPath = path.resolve(__dirname, './app/index.jsx');
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+const cssvariables = require('postcss-css-variables');
+const customMedia = require('postcss-custom-media');
+const imports = require('postcss-import');
+const nested = require('postcss-nested');
+const colorFunction = require('postcss-color-function');
 
 module.exports = {
-  devtool: 'source-map',
-  entry: mainPath,
+  devtool: 'eval',
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    './src/index.jsx'
+  ],
   output: {
-    path: path.join(__dirname, 'build'),
-    filename: 'bundle.js',
+    path: __dirname + '/build',
+    filename: '[name].js',
     publicPath: '/build/'
   },
   module: {
     loaders: [
       {
         test: /\.jsx?$/,
-        exclude: /node_modules/,
+        exclude: /(node_modules)/,
         loader: 'babel',
         query: {
           presets: ['react', 'es2015']
         }
       },
       { test: /\.css$/,
-        loader: "style!css"
+        loader: 'style!css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]!postcss'
       },
-      {
+      { test: /\.jpg$|\.png$|\.svg$/,
+        exclude: /node_modules/,
+        loader: 'file-loader?[name].[hash].[ext]'
+      }
+      ,{
         test: /\.scss$/,
         loaders: ["style", "css?sourceMap", "sass?sourceMap"]
-      },
-      { test: /\.jpg$/,
-        loader: "url-loader?limit=1000"
       }
     ]
   },
+  postcss: function () { return [ autoprefixer, imports, nested, customMedia, cssvariables ] },
   plugins: [
     new HtmlWebpackPlugin({
-      template: __dirname + "/app/index.tmpl.html"
+      template: __dirname + '/src/index.tmpl.html',
+      filename: './index.html'
     }),
     new webpack.optimize.UglifyJsPlugin({
       minimise: true,
       compress: {
         warnings: false
       }
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
     })
-  ]
+  ],
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  }
 };
