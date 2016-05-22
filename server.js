1d31809e7b45818871fb802c53b7bcc769dbe0d2
SeanSassenrath <sean.sassenrath@gmail.com>
2016-05-21 23:27:00 -0700

    Trying to console log in heroku

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const Server = require('./app.js');
 const port = (process.env.PORT || 8080);
 const app = Server.app();
 
+console.log("is this showing up?")
+
 if (process.env.NODE_ENV !== 'production') {
   const webpack = require('webpack');
   const webpackDevMiddleware = require('webpack-dev-middleware');
