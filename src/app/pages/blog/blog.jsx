9bfc95c746c4e01d378d72b21611acaf6f1cfbd5
SeanSassenrath <sean.sassenrath@gmail.com>
2016-09-25 20:25:55 -0700

    Add blog shell component / Add Contentful package

---
+++ src/app/pages/blog/blog.jsx
@@ -0,0 +1,16 @@
+import React, { Component, PropTypes } from 'react';
+import CSSModules from 'react-css-modules';
+import styles from './blog.css';
+
+class Blog extends Component {
+
+  render() {
+    return (
+      <div>
+        Test
+      </div>
+    )
+  }
+}
+
+default export CSSModules(Blog, styles);
