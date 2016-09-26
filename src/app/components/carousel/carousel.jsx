b96c7f226f6fdb46d7ac357bcc3721bba8fb89df
SeanSassenrath <sean.sassenrath@gmail.com>
2016-09-25 22:09:59 -0700

    Make carousel responsive

--- src/app/components/carousel/carousel.jsx
+++ src/app/components/carousel/carousel.jsx
@@ -37,7 +37,26 @@ class Carousel extends Component {
       slidesToShow: 4,
       slidesToScroll: 1,
       prevArrow: <SlickPrevArrow />,
-      nextArrow: <SlickNextArrow />
+      nextArrow: <SlickNextArrow />,
+      responsive: [{
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        }
+      }, {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        }
+      }, {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }]
     }
     return(
       <div>
