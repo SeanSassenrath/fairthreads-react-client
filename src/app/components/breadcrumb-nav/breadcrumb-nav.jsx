b4ab40cbf92eb0bf397e20b26dfc2420f4e06576
Sean Sassenrath <sean.sassenrath@gmail.com>
2016-12-23 23:46:35 -0800

    Make Breadcrumb Nav SFC

--- src/app/components/breadcrumb-nav/breadcrumb-nav.jsx
+++ src/app/components/breadcrumb-nav/breadcrumb-nav.jsx
@@ -1,27 +1,20 @@
 import React, { Component, PropTypes } from 'react';
 
-class BreadcrumbNav extends Component {
-
-  render() {
-    return(
-      <div className="row">
-        <div>
-        </div>
-        <div>
-          <div>
-            <select id="category" onChange={() => addCategoryFilter(document.getElementById('category').value)}>
-              <option disabled>CATEGORY</option>
-              {
-                categoryList ? categoryList.map(function(category) {
-                  return (
-                    <option value={category}>{category}</option>
-                  )
-                }) : null
-              }
-            </select>
-          </div>
-        </div>
+const BreadcrumbNav = () => {
+  return(
+    <div className="row">
+      <div>
+        <select id="category" onChange={() => addCategoryFilter(document.getElementById('category').value)}>
+          <option disabled>CATEGORY</option>
+          {
+            categoryList ? categoryList.map(function(category) {
+              return (
+                <option value={category}>{category}</option>
+              )
+            }) : null
+          }
+        </select>
       </div>
-    )
-  }
+    </div>
+  )
 }
