a8bb38dacc518a88693b4341ef2215ca7b486768
Sean Sassenrath <sean.sassenrath@gmail.com>
2016-12-28 22:29:41 -0800

    Revert ProductCard SFC refactor

--- src/app/components/product-card/product-card.jsx
+++ src/app/components/product-card/product-card.jsx
@@ -3,41 +3,40 @@ import { Link } from 'react-router';
 import CSSModules from 'react-css-modules';
 import styles from './product-card.css';
 
-const ProductCard = ({ product }) => {
-return (
-  <div id={product._id} styleName="product">
-    <a href={product.vendUrl} target="_blank" rel="noopener noreferrer">
-      {/* <Link to={`/product/${product._id}`}> */}
-      <div styleName="img-container">
-        <img
-          src={product.imageOriginal}
-          style={{ objectFit: product.objectFit }}
-          role="presentation"
-        />
-      </div>
-      <div styleName="description">
-        <h5>{product.name}</h5>
-        <h5 styleName="brand">{product.brand}</h5>
-        {product.salePrice
-          ?
-            <div>
-              <h6 styleName="old-price">{`$${Math.ceil(product.price)}`}</h6>
-              <h6 styleName="sale-price">{`$${Math.ceil(product.salePrice)}`}</h6>
-            </div>
-          :
-            <div>
-              <h6 styleName="reg-price">{`$${Math.ceil(product.price)}`}</h6>
-            </div>
-          }
-      </div>
-    </a>
-    {/* </Link> */}
-  </div>
-  )
-};
+class ProductCard extends Component {
 
-ProductCard.propTypes = {
-  product: PropTypes.element,
-};
+  render() {
+    let {
+      product,
+    } = this.props;
+
+    return(
+      <div id={product._id} styleName="product">
+        <a href={product.vendUrl} target='_blank'>
+        {/*<Link to={`/product/${product._id}`}>*/}
+            <div styleName="img-container">
+              <img src={product.imageOriginal} style={{objectFit: product.objectFit}} />
+            </div>
+            <div styleName="description">
+              <h5>{product.name}</h5>
+              <h5 styleName="brand">{product.brand}</h5>
+                {product.salePrice
+                  ?
+                    <div>
+                      <h6 styleName="old-price">{`$${Math.ceil(product.price)}`}</h6>
+                      <h6 styleName="sale-price">{`$${Math.ceil(product.salePrice)}`}</h6>
+                    </div>
+                  :
+                    <div>
+                      <h6 styleName="reg-price">{`$${Math.ceil(product.price)}`}</h6>
+                    </div>
+                }
+            </div>
+          </a>
+          {/*</Link>*/}
+        </div>
+      )
+  }
+}
 
 export default CSSModules(ProductCard, styles);
