d8c2cf1c47319cf31a449e106cde5bc072cf4ab8
Sean Sassenrath <sean.sassenrath@gmail.com>
2017-02-26 17:21:48 -0800

    Add set timeout to loading

--- src/app/components/product-card/product-card.jsx
+++ src/app/components/product-card/product-card.jsx
@@ -1,44 +1,79 @@
 import React, { Component, PropTypes } from 'react';
+import autobind from 'react-autobind';
 import { Link } from 'react-router';
 import CSSModules from 'react-css-modules';
 import styles from './product-card.css';
 
-const renderImageArea = (product, isLoading) => {
-  const imagePlaceholder = isLoading ? styles['image-placeholder-active'] : styles['image-placeholder'];
-  return (
-    <div className={styles['image-container']}>
-      <div className={imagePlaceholder} />
-    </div>
-  );
-};
+class ProductCard extends Component {
+  constructor(props) {
+    super(props);
+    autobind(this);
+    this.state = {
+      loading: true,
+    };
+  }
 
-const ProductCard = (props) => {
-  const { product, isLoading } = props;
+  componentWillUpdate(prevProps) {
+    if (prevProps.isLoading && !this.props.isLoading) {
+      setTimeout(() => {
+        console.log(prevProps);
+        console.log(this.props.isLoading);
+        this.setState({ loading: true });
+      })
+    } else if (!prevProps.isLoading && this.props.isLoading) {
+      setTimeout(() => {
+        this.setState({ loading: false });
+        console.log(prevProps);
+        console.log(this.props.isLoading);
+      }, 1000);
+    }
+  }
 
-  return (
-    <div id={product._id} className={styles.container}>
-      <a href={product.vendUrl} target="_blank" rel="noopener noreferrer">
-        { renderImageArea(product, isLoading) }
-        <div>
-          <span className={styles.brand}>{product.brand}</span>
-          <span className={styles.name}>{product.name}</span>
-          {product.salePrice
-            ?
-              <div className={styles['price-container']}>
-                <span className={styles['old-price']}>{`$${Math.ceil(product.price)}`}</span>
-                <span>{`$${Math.ceil(product.salePrice)}`}</span>
-              </div>
-            :
-              <div className={styles['price-container']}>
-                <span>{`$${Math.ceil(product.price)}`}</span>
-              </div>
-            }
-        </div>
-      </a>
-    </div>
-  );
-};
+  renderImageArea(product, isLoading) {
+    const imagePlaceholder = isLoading ? styles['image-placeholder-active'] : styles['image-placeholder'];
+    return (
+      <div className={styles['image-container']}>
+        <div className={imagePlaceholder} />
+        { !isLoading ?
+          <img
+            src={product.imageOriginal}
+            style={{ objectFit: product.objectFit }}
+            role="presentation"
+            className={styles.image}
+          />
+          : null
+        }
+      </div>
+    );
+  }
 
+  render() {
+    const { product } = this.props;
+    const { loading } = this.state;
+    return (
+      <div id={product._id} className={styles.container}>
+        <a href={product.vendUrl} target="_blank" rel="noopener noreferrer">
+          {this.renderImageArea(product, loading) }
+          <div>
+            <span className={styles.brand}>{product.brand}</span>
+            <span className={styles.name}>{product.name}</span>
+            {product.salePrice
+              ?
+                <div className={styles['price-container']}>
+                  <span className={styles['old-price']}>{`$${Math.ceil(product.price)}`}</span>
+                  <span>{`$${Math.ceil(product.salePrice)}`}</span>
+                </div>
+              :
+                <div className={styles['price-container']}>
+                  <span>{`$${Math.ceil(product.price)}`}</span>
+                </div>
+              }
+          </div>
+        </a>
+      </div>
+    );
+  }
+}
 
 ProductCard.propTypes = {
   product: PropTypes.ReactElement,
